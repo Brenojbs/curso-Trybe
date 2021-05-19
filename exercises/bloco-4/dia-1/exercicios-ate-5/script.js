1ffe8d9264fa67ddbb244074d083830e2949a0b9
José Breno <brenojbs@hotmail.com>
2021-05-19 19:16:27 -0300

    Exercicios ate 4 do dia 4.1

---
+++ exercises/bloco-4/dia-1/exercicios-ate-5/script.js
@@ -0,0 +1,27 @@
+let num1 = -5
+let num2 = 10
+let num3 = 15
+
+
+if (num1 > num2){
+  console.log(num1)
+} else {
+  console.log(num2)
+}
+
+if (num1 > num2){
+  console.log(num1)
+} else if(num2 < num3) {
+  console.log(num3)
+} else{
+  console.log(num2)   
+}
+
+if (num1 > 0) {
+  console.log ("Positive")
+} else if (num1 < 0) {
+  console.log("Negative")
+} else {
+  console.log("Zero")
+}
+
