34c7a2b6de1bdccf2c61ca53f4201c0f482815b5
José Breno <brenojbs@hotmail.com>
2021-05-25 12:25:48 -0300

    função 1 exercicio criado dia 4.4

--- exercises/bloco-4/dia-4/exercicio2.js
+++ exercises/bloco-4/dia-4/exercicio2.js
@@ -1,15 +1,19 @@
 function verificaPalindromo(palavra) {
     let palindromo = false;
     for(let index = 0; index < palavra.length; index += 1) {
-        for (let index2 = palavra.length; index2 >= 0; index -= 1) {
-            if (palavra[index] === palavra[index2]) {
-                palindromo = true;
-            } else {
-                palindromo = false;
-            }
+        let palavra1 = palavra[index];
+        
+        for (let index2 = palavra.length; index2 >= 0; index2 -= 1) {
+            let palavra2 = palavra[index2];
         }
     }
-    return palindromo
+    if (palavra1 === palavra2) {
+        palindromo = true;
+    } else {
+        palindromo = false;
+    }
+    return palindromo;
 }
-let verificado = verificaPalindromo('arara')
+let verificado = verificaPalindromo('arara');
+
 console.log(verificado)
