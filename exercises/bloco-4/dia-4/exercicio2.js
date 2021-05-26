345a1d0fa3675a3a9f427d3b190f7fa2bc445930
José Breno <brenojbs@hotmail.com>
2021-05-26 18:22:31 -0300

    Exercicio 5-1

--- exercises/bloco-4/dia-4/exercicio2.js
+++ exercises/bloco-4/dia-4/exercicio2.js
@@ -1,13 +1,12 @@
 function verificaPalindromo(palavra) {
-    let palindromo = false;
-    for(let index = 0; index < palavra.length; index += 1) {
-        let palavra1 = palavra[index];
-        
-        for (let index2 = palavra.length; index2 >= 0; index2 -= 1) {
-            let palavra2 = palavra[index2];
+    let palindromo = false; 
+    let palavra2 = '';   
+    for (let index2 = palavra.length -1; index2 >= 0; index2 -= 1) {
+        let palavra1 = palavra[index2];
+         palavra2.push(palavra1);
+         console.log(palavra2);
         }
-    }
-    if (palavra1 === palavra2) {
+    if (palavra === palavra2) {
         palindromo = true;
     } else {
         palindromo = false;
