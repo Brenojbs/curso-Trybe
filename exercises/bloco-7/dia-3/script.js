1c511ed321928fcbfe9e0489ae8e2a3714e6a0a3
José Breno <brenojbs@hotmail.com>
2021-06-21 22:03:12 -0300

    Exercicio part 1 dia7-3 Testes unitarios

--- exercises/bloco-7/dia-3/script.js
+++ exercises/bloco-7/dia-3/script.js
@@ -1,1 +1,30 @@
-function
+const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+
+// assert.strictEqual(sum(4, 5), 9)
+
+// assert.strictEqual(sum(0, 0), 0)
+
+// assert.strictEqual(sum(4, '5'), 9)
+
+
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+let array = [1, 2, 4]
+let ar = [1, 2, 3, 4]
+
