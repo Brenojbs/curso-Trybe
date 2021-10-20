a343cf65fbb5978fe927b8ff64b8d7c507630e6c
Brenojbs <brenojbs@hotmail.com>
2021-10-20 11:52:59 -0300

    Bloco 20, dia 1

---
+++ Modulo-back-end/bloco-20/dia-1/functions-string.sql
@@ -0,0 +1,2 @@
+-- Replace() recebe 3 argumentos. 1° uma string, 2° a string a ser alterada, 3° o que sera adcionada a string inicial.
+SELECT REPLACE('Oi, eu sou uma string', 'string', 'cadeia de caracteres');
