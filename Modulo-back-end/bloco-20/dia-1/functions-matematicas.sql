f9aa5d7526cf7300fa9dfe3f86fc5a5b511810aa
Brenojbs <brenojbs@hotmail.com>
2021-10-20 15:52:55 -0300

    functions about mysql

---
+++ Modulo-back-end/bloco-20/dia-1/functions-matematicas.sql
@@ -0,0 +1,51 @@
+-- Functions numeric value!
+
+SELECT 5 + 5;
+SELECT 5 - 5;
+SELECT 5 * 5;
+SELECT 5 / 5;
+--
+SELECT rental_duration + rental_rate FROM sakila.film LIMIT 10;
+SELECT rental_duration - rental_rate FROM sakila.film LIMIT 10;
+SELECT rental_duration / rental_rate FROM sakila.film LIMIT 10;
+SELECT rental_duration * rental_rate FROM sakila.film LIMIT 10;
+--
+SELECT 10 DIV 3; -- 3
+SELECT 10 DIV 2; -- 5
+SELECT 14 DIV 3; -- 4
+SELECT 13 DIV 2; -- 6
+--
+SELECT 10 MOD 3; -- 1
+SELECT 10 MOD 2; -- 0
+SELECT 14 MOD 3; -- 2
+SELECT 13 MOD 2; -- 1
+SELECT 10.5 MOD 2; -- 0.5, ou seja, 2 + 2 + 2 + 2 + 2 = 10, restando 0.5
+--
+-- Podemos omitir ou especificar quantas casas decimais queremos
+SELECT ROUND(10.4925); -- 10
+SELECT ROUND(10.5136); -- 11
+SELECT ROUND(-10.5136); -- -11
+SELECT ROUND(10.4925, 2); -- 10.49
+SELECT ROUND(10.4925, 3); -- 10.493
+--
+SELECT CEIL(10.51); -- 11
+SELECT CEIL(10.49); -- 11
+SELECT CEIL(10.2); -- 11
+--
+SELECT FLOOR(10.51); -- 10
+SELECT FLOOR(10.49); -- 10
+SELECT FLOOR(10.2); -- 10
+--
+SELECT POW(2, 2); -- 4
+SELECT POW(2, 4); -- 16
+--
+SELECT SQRT(9); -- 3
+SELECT SQRT(16); -- 4
+--
+-- Para gerar um valor aleatório entre 0 e 1:
+SELECT RAND();
+
+-- Para gerar um valor entre 7 e 13:
+SELECT ROUND(7 + (RAND() * 6));
+
+-- O cálculo que é feito é o seguinte: (7 + (0.0 a 1.0 * 6))
---
+++ Modulo-back-end/bloco-20/dia-1/functions-time.sql
@@ -0,0 +1,38 @@
+-- functios of time!
+
+-- Para gerar um valor aleatório entre 0 e 1:
+SELECT RAND();
+
+-- Para gerar um valor entre 7 e 13:
+SELECT ROUND(7 + (RAND() * 6));
+
+-- O cálculo que é feito é o seguinte: (7 + (0.0 a 1.0 * 6))
+
+--
+
+-- 30, ou seja, a primeira data é 30 dias depois da segunda
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
+
+-- -30, ou seja, a primeira data é 30 dias antes da segunda
+SELECT DATEDIFF('2020-01-01', '2020-01-31');
+
+-- -01:00:00, ou seja, há 1 hora de diferença entre os horários
+SELECT TIMEDIFF('08:30:10', '09:30:10');
+
+-- -239:00:00, ou seja, há uma diferença de 239 horas entre as datas
+SELECT TIMEDIFF('2021-08-11 08:30:10', '2021-08-01 09:30:10');
+
+--
+
+SELECT DATE(data_cadastro); -- YYYY-MM-DD
+SELECT YEAR(data_cadastro); -- Ano
+SELECT MONTH(data_cadastro); -- Mês
+SELECT DAY(data_cadastro); -- Dia
+SELECT HOUR(data_cadastro); -- Hora
+SELECT MINUTE(data_cadastro); -- Minuto
+SELECT SECOND(data_cadastro); -- Segundo
+
+--
+
+SELECT YEAR(CURRENT_DATE()); -- retorna o ano atual
+SELECT HOUR(NOW()); -- retorna a hora atual
